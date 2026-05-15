f29a6895c75763de6ded2d166cf9b55fa60633c2
Ali Rouhi <thealirouhi@gmail.com>
2026-05-15 16:22:16 +0330

    Implemented loading of the dictionaries.

--- main.cpp
+++ main.cpp
@@ -7,22 +7,42 @@
 using namespace std;
 
 // loading dictionary
-void loadDictionary(map<string, bool>& dictionary, int n);
+void loadDictionary(map<string, bool> &dictionary, int n);
 
 // backtracking helper (to be redesigned later)
-bool goForward(const string& word, const map<string, bool>& dictionary);
+bool goForward(const string &word, const map<string, bool> &dictionary);
 
 // find valid words for a query
-vector<string> findValidCombinations(const vector<char>& letters,
-                                     const map<string, bool>& dictionary);
+vector<string> findValidCombinations(const vector<char> &letters,
+                                     const map<string, bool> &dictionary);
 
 // handle one query
-void processQuery(const map<string, bool>& dictionary);
+void processQuery(const map<string, bool> &dictionary);
 
 // main driver
 void runGame();
 
-int main() {
+void loadDictionary(map<string, bool> &dictionary, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        string word;
+        cin >> word;
+        dictionary[word] = true;
+    }
+}
+
+void runGame()
+{
+    int n;
+    cin >> n;
+
+    map<string, bool> dictionary;
+    loadDictionary(dictionary, n);
+}
+
+int main()
+{
     runGame();
     return 0;
 }
